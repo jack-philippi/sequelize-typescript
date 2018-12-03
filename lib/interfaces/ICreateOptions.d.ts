2caa1b9c81a79929e747c08a373a805cf650bd7e
jack-philippi <42924255+jack-philippi@users.noreply.github.com>
2018-12-03 14:58:58 +1100

    Update ICreateOptions.d.ts

--- lib/interfaces/ICreateOptions.d.ts
+++ lib/interfaces/ICreateOptions.d.ts
@@ -1,11 +1,20 @@
-import { InstanceSaveOptions } from 'sequelize';
-import { IBuildOptions } from "./IBuildOptions";
+import {InstanceSaveOptions} from 'sequelize';
+import {IBuildOptions} from "./IBuildOptions";
+
 /**
  * Options for Model.create method
  */
 export interface ICreateOptions extends IBuildOptions, InstanceSaveOptions {
-    /**
-     * On Duplicate
-     */
-    onDuplicate?: string;
+
+  /**
+   * On Duplicate
+   */
+  onDuplicate?: string;
+
+  /**
+   * Enable or disable hooks being triggered from this call
+   * @default false
+   */
+  hooks?: boolean;
+
 }
